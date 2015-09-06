9dca8a1aafcf71be1a49d94166ca1f7c350e9664
David Burkhart <David.Burkhart@gmx.de>
2015-09-06 21:57:10 +0200

    clean all when ant done

--- quickclean/src/cleanbutton/Activator.java
+++ quickclean/src/cleanbutton/Activator.java
@@ -1,5 +1,8 @@
 package cleanbutton;
 
+import org.eclipse.debug.core.DebugEvent;
+import org.eclipse.debug.core.DebugPlugin;
+import org.eclipse.debug.core.IDebugEventSetListener;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
@@ -10,6 +13,19 @@ public class Activator extends AbstractUIPlugin {
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
+
+		DebugPlugin.getDefault().addDebugEventListener(new IDebugEventSetListener() {
+			@Override
+			public void handleDebugEvents(DebugEvent[] events) {
+				for (DebugEvent event : events) {
+					if (event.getKind() == DebugEvent.TERMINATE
+							&& event.getSource().getClass().getName().startsWith("org.eclipse.ant")) {
+						System.err.println("Ant done!");
+						new ScheduleCleanAll().runClean();
+					}
+				}
+			}
+		});
 	}
 
 	public void stop(BundleContext context) throws Exception {
--- quickclean/src/cleanbutton/handlers/CleanAllHandler.java
+++ quickclean/src/cleanbutton/handlers/CleanAllHandler.java
@@ -3,55 +3,13 @@ package cleanbutton.handlers;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.resources.IncrementalProjectBuilder;
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.resources.WorkspaceJob;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.actions.GlobalBuildAction;
-import org.eclipse.ui.handlers.HandlerUtil;
-import org.eclipse.ui.progress.IProgressConstants2;
+
+import cleanbutton.ScheduleCleanAll;
 
 public class CleanAllHandler extends AbstractHandler {
 
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
-		clean(window);
+		new ScheduleCleanAll().runClean();
 		return null;
-	}
-
-	private void clean(IWorkbenchWindow window) {
-		WorkspaceJob cleanJob = new WorkspaceJob("Clean and build workspace") {
-			@Override
-			public boolean belongsTo(Object family) {
-				return ResourcesPlugin.FAMILY_MANUAL_BUILD.equals(family);
-			}
-
-			@Override
-			public IStatus runInWorkspace(IProgressMonitor monitor) throws CoreException {
-				return cleanAll(window, monitor);
-			}
-		};
-		cleanJob.setRule(ResourcesPlugin.getWorkspace().getRuleFactory().buildRule());
-		cleanJob.setUser(true);
-		cleanJob.setProperty(IProgressConstants2.SHOW_IN_TASKBAR_ICON_PROPERTY, Boolean.TRUE);
-		cleanJob.schedule();
-	}
-
-	private IStatus cleanAll(IWorkbenchWindow window, IProgressMonitor monitor) throws CoreException {
-		monitor.beginTask("Clean all projects", 2);
-
-		monitor.subTask("Cleaning...");
-		ResourcesPlugin.getWorkspace().build(IncrementalProjectBuilder.CLEAN_BUILD, monitor);
-		monitor.worked(1);
-
-		monitor.subTask("Building...");
-		new GlobalBuildAction(window, IncrementalProjectBuilder.INCREMENTAL_BUILD).doBuild();
-		monitor.worked(1);
-
-		return Status.OK_STATUS;
 	}
 }
